33a90ef256a70fb7703cf01bfafd41b652aedcfe
Sydney Porter <Syd.porter03@gmail.com>
2025-06-15 21:45:48 -0400

    Added some information on the about page with color changing for dark/light mode

--- blogging-website/app/about/page.js
+++ blogging-website/app/about/page.js
@@ -4,7 +4,23 @@ export default function AboutPage() {
     return (
         <div>
             <NavBar />
-            <h1 className="text-9xl"> About </h1>
+            <h1 className="text-5xl pt-60 ml-50 text-pink-600"> Hey! I'm Sydney</h1>
+            <div className="w-150 ml-50">
+                <h2 className="text-2xl pt-7 hidden dark:block"> And welcome to my website! This is a passion project, dedicated to developing my skills with topics that I love!
+                    Echo is a way to get information out to the masses. People want to connect with people that share their interests, whatever that may be.
+                    It could be crochet, geography, or really anything you could imagine.
+                </h2>
+                <h2 className="text-2xl pt-7 text-black dark:hidden"> And welcome to my website! This is a passion project, dedicated to developing my skills with topics that I love!
+                    Echo is a way to get information out to the masses. People want to connect with people that share their interests, whatever that may be.
+                    It could be crochet, geography, or really anything you could imagine.
+                </h2>
+                <h2 className="text-2xl pt-7 hidden dark:block"> The project is still developing as, of course, I am always working out kinks and wanting any feeback that
+                    could provide a better user experience for the viewer.
+                </h2>
+                <h2 className="text-2xl pt-7 text-black dark:hidden"> The project is still developing as, of course, I am always working out kinks and wanting any feeback that
+                    could provide a better user experience for the viewer.
+                </h2>
+            </div>
         </div>
     );
 }
