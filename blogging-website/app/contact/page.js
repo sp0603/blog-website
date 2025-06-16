caf3a54d077d5bfa65a01e58ebe9902798015971
Sydney Porter <Syd.porter03@gmail.com>
2025-06-15 22:46:55 -0400

    Added some content to the contact page, will revist later

--- blogging-website/app/contact/page.js
+++ blogging-website/app/contact/page.js
@@ -4,7 +4,14 @@ export default function ContactPage() {
     return (
         <div>
             <NavBar />
-            <h1 className="text-9xl"> Contact </h1>
+            <h1 className="text-5xl pt-60 ml-50 text-pink-600"> Contact Me! </h1>
+            <div className="w-150 ml-50">
+                <h2 className="text-2xl pt-7 hidden dark:block">
+                    I am always open to ideas that would help develop this website into something people
+                    would really want to experience with and push it even further for the future. Who knows? I also would love it
+                    if people notice anything that looks out of place or is 
+                </h2>
+            </div>
         </div>
     );
 }
