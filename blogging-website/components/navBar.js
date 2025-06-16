c29fbd937afe77d827c9a1270c038cdd3d450fe4
Sydney Porter <Syd.porter03@gmail.com>
2025-06-15 21:56:05 -0400

    quick little change with hover state for navbar

--- blogging-website/components/navBar.js
+++ blogging-website/components/navBar.js
@@ -3,11 +3,11 @@ export default function NavBar() {
     return (
         <nav className="inline">
             <ul className="flex justify-end text-right space-x-15 text-l font-bold m-5 bg">
-                <li><Link href="/about">About</Link></li>
-                <li><Link href="/contact">Contact</Link></li>
-                <li><Link href="/"> Echo</Link></li>
-                <li><Link href="/signup">Sign up</Link></li>
-                <li><Link href="/login"> Log In</Link></li>
+                <li className="hover:text-pink-600"><Link href="/about">About</Link></li>
+                <li className="hover:text-pink-600"><Link href="/contact">Contact</Link></li>
+                <li className="hover:text-pink-600"><Link href="/"> Echo</Link></li>
+                <li className="hover:text-pink-600"><Link href="/signup">Sign up</Link></li>
+                <li className="hover:text-pink-600"><Link href="/login"> Log In</Link></li>
             </ul>
         </nav>
     );
