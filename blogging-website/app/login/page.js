159e1c676aa7da92fa550381d02bb23dfda74e2e
Sydney Porter <Syd.porter03@gmail.com>
2025-06-18 13:18:17 -0400

    login and signup pages not yet functional

--- blogging-website/app/login/page.js
+++ blogging-website/app/login/page.js
@@ -1,10 +1,23 @@
 import NavBar from "@/components/navBar";
 
-export default function login(){
-    return(
+export default function login() {
+    return (
         <div>
-            <NavBar />
-            <h1 className="text-9xl"> Log In</h1>
+            < NavBar />
+            <div className="flex justify-center mt-50">
+                <form className="flex flex-col space-y-4 p-4 w-100">
+                    <h1 className="text-xl font-semibold text-center text-black"> Log In</h1>
+                    <label className="text-sm font-semibold text-center text-black">Email</label>
+                    <input type="email" placeholder="Email" className="p-2 border rounded" />
+
+                    <label className="text-sm font-semibold text-center text-black">Password</label>
+                    <input type="password" placeholder="Password" className="p-2 border rounded" />
+
+                    <button type="submit" className="bg-black text-white p-2 rounded hover:bg-pink-600">
+                        Log in
+                    </button>
+                </form>
+            </div>
         </div>
     );
 }
--- blogging-website/app/signup/page.js
+++ blogging-website/app/signup/page.js
@@ -6,17 +6,17 @@ export default function () {
             < NavBar />
             <div className="flex justify-center mt-50">
                 <form className="flex flex-col space-y-4 p-4 w-100">
-                    <h1 className="text-xl font-semibold text-center"> Sign Up</h1>
-                    <label className="text-sm font-semibold text-center">Email</label>
+                    <h1 className="text-xl font-semibold text-center text-black"> Sign Up</h1>
+                    <label className="text-sm font-semibold text-center text-black">Email</label>
                     <input type="email" placeholder="Email" className="p-2 border rounded" />
 
-                    <label className="text-sm font-semibold text-center">Password</label>
+                    <label className="text-sm font-semibold text-center text-black">Password</label>
                     <input type="password" placeholder="Password" className="p-2 border rounded" />
 
-                    <label className="text-sm font-semibold text-center">Username</label>
+                    <label className="text-sm font-semibold text-center text-black">Username</label>
                     <input type="text" placeholder="Username" className="p-2 border rounded" />
 
-                    <button type="submit" className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600">
+                    <button type="submit" className="bg-black text-white p-2 rounded hover:bg-pink-600">
                         Sign up
                     </button>
                 </form>
